cbca68a287e39f63cb3d18e6c33f1695f7f383f4
SaraMolas <saramolas18@gmail.com>
2025-09-28 10:32:38 +0100

    updated run_script.py inputs

--- examples/run_script.py
+++ examples/run_script.py
@@ -30,7 +30,7 @@ def main():
     # generate trajectory and make plots 
     traj = generate_trajectory(track_length = 1.0, dt = 0.005, duration_s = 300.0, theta = 1.0, mu = 0.0, sigma = 0.4, v0 = 0.0) 
 
-    fig1, _ =  occupancy_plot(time = traj["time"], pos = traj["pos"], track_length = traj["meta"]["track_length"], n_bins = 50)
+    fig1, _ =  occupancy_plot(pos = traj["pos"], track_length = traj["meta"]["track_length"], n_bins = 50)
     
     fig2, _ = plot_position(traj["time"], pos = traj["pos"], track_length = traj["meta"]["track_length"])
     
@@ -38,7 +38,7 @@ def main():
     n_cells = 20
     centers = np.linspace(0.1, 0.9, n_cells)
     sigmas = np.full(n_cells, 0.1)
-    peak_rates = np.random.uniform(5, 20, n_cells)
+    peak_rates = np.random.uniform(10, 20, n_cells)
     baseline_rate = 0.5
     
     # parameters for noise cells
@@ -47,24 +47,27 @@ def main():
     max_noise_rate = 1.0
 
     # generate spikes and rates for place cells
-    spikes, spike_times, spike_pos, spike_counts = generate_place_cell_spikes(centers, sigmas, peak_rates, baseline_rate,
-                               traj['pos'], traj['time'], traj['dt']) 
+    spikes, spike_times, spike_pos, spike_counts = generate_place_cell_spikes(centers = centers, sigma_pf = sigmas, peak_rates = peak_rates, 
+    baseline_rate = baseline_rate, pos = traj['pos'], time = traj['time'], duration_s = 300.0, dt = 0.005) 
 
-    empirical_rate_maps, bin_centers = compute_empirical_rate_maps(n_bins = 100, track_length = traj["meta"]["track_length"], pos = traj["pos"], dt = traj["dt"], 
-                                spike_postions = spike_pos) 
+    #  generate empirical ratemaps (based on spikes and trajectory) and theoretical ratemaps (based on place field parameters)
+    empirical_rate_maps, bin_centers = compute_empirical_rate_maps(n_bins = 100, track_length = traj["meta"]["track_length"],
+    pos = traj["pos"], dt = 0.005, spike_positions = spike_pos) 
     
-    theoretical_rates, x = compute_theoretical_rate_maps(track_length=traj["meta"]["track_length"], centers = centers, sigma_pf = sigmas, peak_rates = peak_rates, 
-                                  baseline_rate = baseline_rate)
-    
+    theoretical_rates, x = compute_theoretical_rate_maps(n_bins = 100, track_length=traj["meta"]["track_length"], centers = centers, sigma_pf = sigmas,
+    peak_rates = peak_rates, baseline_rate = baseline_rate)
+
     # generate spikes for noise cells
-    noise_spikes = generate_noise_cell_spikes(n_noise = n_noise, min_rate = min_noise_rate, max_rate = max_noise_rate,  time = traj['time'], dt = traj['dt'])
-    
+    noise_spikes = generate_noise_cell_spikes(n_noise = n_noise, min_rate = min_noise_rate, max_rate = max_noise_rate,  
+        duration_s = traj["meta"]["duration_s"], dt = 0.005)
+
     # save dataset (trajectory only for now) and additional arrays
-    save_dataset({"time": traj["time"], "pos": traj["pos"], "vel": traj["vel"], "meta": traj["meta"]}, args.out)
+    save_dataset({"time": traj["time"], "pos": traj["pos"], "vel": traj["velocity"], "meta": traj["meta"], 
+    "spikes": traj["place_cell_spikes"], "noise_spikes": traj["noise_cell_spikes"]}, args.out)
     print("Saved dataset to", args.out)
 
     # create figures with spike raster and empirical vs theoretical rate maps of place cells 
-    fig3, _ = plot_spike_raster(spike_times = spike_times, duration = traj["meta"]["duration"], centers = centers)
+    fig3, _ = plot_spike_raster(spike_times = spike_times, duration = traj["meta"]["duration_s"], centers = centers)
 
     fig4, _ = plot_empirical_vs_theoretical_rate(empirical_rates = empirical_rate_maps, theoretical_rates = theoretical_rates, 
                                        bin_centers = bin_centers, centers = centers, peak_rates = peak_rates, 
