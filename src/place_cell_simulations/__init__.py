62cc8686cc223f74e18f57aeba2b3c9e82483b08
SaraMolas <saramolas18@gmail.com>
2025-09-28 01:08:53 +0100

    renamed notebook

--- src/place_cell_simulations/__init__.py
+++ src/place_cell_simulations/__init__.py
@@ -1,7 +1,7 @@
 from .movement import generate_trajectory
 from .spikes import generate_place_cell_spikes, generate_noise_cell_spikes
 from .utils import compute_empirical_rate_maps, compute_theoretical_rate_maps, save_dataset
-from .plots import plot_position, plot_spike_raster, plot_empirical_vs_theoretical_rate, plot_rate_maps
+from .plots import occupancy_plot, plot_position, plot_spike_raster, plot_empirical_vs_theoretical_rate, plot_rate_maps
 
 __all__ = [
     "generate_trajectory",
